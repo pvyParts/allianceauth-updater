c52c04200f3b4ee928ec6049026714f4f7f29db2
AaronKable <aaronkable@gmail.com>
2020-09-17 14:31:27 +0800

    update

--- setup.py
+++ setup.py
@@ -3,34 +3,30 @@ from setuptools import find_packages, setup
 from auth_updater import VERSION
 
 install_requires = [
-    'allianceauth>=2.7.5',
-    'celery>=4.4.2,<5.0.0',
-    'django>=2.2.16,<3.0.0',  #3.0.0 not working with auth
-    'mysqlclient',
-    'dnspython',
-    'passlib',
-    'requests>=2.9.1,<3.0.0',
-    'bcrypt',
-    'python-slugify>=1.2',
-    'requests-oauthlib',
-    'semantic_version',
-    'packaging>=20.1,<21',
+    'allianceauth==2.8.0a1',
+    'celery==4.4.7',
+    'django==2.2.16',  #3.0.0 not working with auth
+    'mysqlclient==2.0.1',
+    'dnspython==2.0.0',
+    'passlib==1.7.2',
+    'requests==2.24.0',
+    'bcrypt==3.2.0',
+    'python-slugify==4.0.0',
+    'requests-oauthlib==1.3.0',
+    'semantic_version==2.8,5',
+    'packaging==20.4',
 
-    'redis>=3.3.1,<4.0.0',
-    'celery>=4.3.0,<5.0.0,!=4.4.4',  # 4.4.4 is missing a dependency
-    'celery_once',
+    'redis==3.5.3',
+    'celery_once==3.0.1',
 
-    'django-bootstrap-form',
-    'django-registration==2.4',
-    'django-sortedm2m',
-    'django-redis-cache>=2.1.0,<3.0.0',
-    'django-celery-beat>=1.1.1,<2.0.0',
+    'django-bootstrap-form==3.4',
+    'django-registration==2.5.2', # incompat 3.0
+    'django-sortedm2m==3.0.2',
+    'django-redis-cache==2.1.1',
+    'django-celery-beat>=1.6.0', # incompat 2.0
 
-    'openfire-restapi',
-    'sleekxmpp',
-    'pydiscourse',
-
-    'django-esi>=2.0.0,<3.0']
+    'django-esi==2.0.0',
+    'bravado==10.6.2']
 
 this_directory = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
