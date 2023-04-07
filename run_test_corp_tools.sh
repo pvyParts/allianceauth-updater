dd72baeb4f1e6214c2c62db9ff1e320eb42a7eb8
Aaron Kable <aaronkable@gmail.com>
2023-04-07 17:06:17 +0800

    add new req file

--- run_test_corp_tools.sh
+++ run_test_corp_tools.sh
@@ -1,9 +1,11 @@
 git clone https://github.com/Solar-Helix-Independent-Transport/allianceauth-corp-tools.git
 cd allianceauth-corp-tools
+pwd
 export DJANGO_SETTINGS_MODULE="tests.test_settings"
 pip install --upgrade pip
 pip install coverage
-pip install ../requirements.txt
+pip install -e .
+pip install -r ../requirements.txt
 pip freeze
 coverage run runtests.py
 coverage report -m
