ee6a4854995108bdc9340543f99825bce0e92796
Aaron Kable <aaronkable@gmail.com>
2023-04-07 18:12:39 +0800

    make no worky nested

--- run_test_alliance_auth.sh
+++ run_test_alliance_auth.sh
@@ -2,6 +2,16 @@ git clone https://gitlab.com/allianceauth/allianceauth.git
 cd allianceauth
 pwd
 export DJANGO_SETTINGS_MODULE="tests.settings_all"
-make setup
+pip install --upgrade pip
+pip install coverage
 pip install -e ".[test]"
-make runtest
+pip install -r ../requirements.txt
+echo "****************************************"
+echo "********** Current Versions! ***********"
+echo "****************************************"
+pip freeze
+echo "****************************************"
+echo "**********   Running Tests!   **********"
+echo "****************************************"
+coverage run runtests.py -v 2 --debug-mode
+coverage report -m
--- run_test_corp_tools.sh
+++ run_test_corp_tools.sh
@@ -2,6 +2,16 @@ git clone https://github.com/Solar-Helix-Independent-Transport/allianceauth-corp
 cd allianceauth-corp-tools
 pwd
 export DJANGO_SETTINGS_MODULE="tests.test_settings"
-make setup
+	pip install --upgrade pip
+	pip install coverage
 pip install -e .
-make runtest
+pip install -r ../requirements.txt
+echo "****************************************"
+echo "********** Current Versions! ***********"
+echo "****************************************"
+pip freeze
+echo "****************************************"
+echo "**********   Running Tests!   **********"
+echo "****************************************"
+coverage run runtests.py -v 2 --debug-mode
+coverage report -m
