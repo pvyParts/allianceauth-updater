8c35bf386dbf246a0182715b5887703ea3e2c70f
Aaron Kable <aaronkable@gmail.com>
2023-04-07 16:22:03 +0800

    add env

--- run_test_corp_tools.sh
+++ run_test_corp_tools.sh
@@ -1,5 +1,6 @@
 git clone https://github.com/Solar-Helix-Independent-Transport/allianceauth-corp-tools.git
 cd allianceauth-corp-tools
+export DJANGO_SETTINGS_MODULE = tests.test_settings
 pip install --upgrade pip
 pip install coverage
 pip install ../requirements.txt
